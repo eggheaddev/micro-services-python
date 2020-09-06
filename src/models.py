510b2112224fa2ec24635ac4ab94fb3612d14d9a
Rivier <rgrullon@ipisdosaj.edu.do>
2020-09-05 23:25:26 -0400

    update:  modify the User Model

--- src/models.py
+++ src/models.py
@@ -1,6 +1,7 @@
-from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
+from flask_sqlalchemy import SQLAlchemy
 import datetime
+import uuid
 
 db = SQLAlchemy()
 ma = Marshmallow()
@@ -11,9 +12,11 @@ def _get_date():
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
+    public_id = db.Column(db.String(50), default=str(uuid.uuid4()), unique=True)
     name = db.Column(db.String(70))
     email = db.Column(db.String(70),unique=True)
-    password = db.Column(db.String(100))
+    password = db.Column(db.String(255))
+    admin = db.Column(db.Boolean, default=False)
     created_at = db.Column(db.Date, default=_get_date)
 
     def __init__(self, name, email, password):
@@ -25,7 +28,7 @@ class User(db.Model):
 
 class UserSchema(ma.Schema):
     class Meta:
-        fields = ('id', 'name', 'email', 'password')
+        fields = ('id', 'public_id', 'name', 'email', 'password')
 
 
 user_schema = UserSchema()
