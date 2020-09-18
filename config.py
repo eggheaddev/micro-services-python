813086f3a3d24f5f9eba6fe074bb2499295c7a1c
slorard <slora@ipisdosaj.edu.do>
2020-09-18 14:27:14 -0400

    fix: config variable

--- config.py
+++ config.py
@@ -8,12 +8,12 @@ class Config:
     """Set Flask configuration from .env file."""
 
     # General Config
-    SECRET_KEY = environ.get('SECRET_KEY')
+    SECRET_KEY = environ['SECRET_KEY']
     if not SECRET_KEY:
         raise ValueError("No SECRET_KEY set for Flask application")
-    ACCESS_TOKEN = environ.get('access_token')
+    ACCESS_TOKEN = environ['ACCESS_TOKEN']
 
     # Database
-    SQLALCHEMY_DATABASE_URI = environ.get("SQLALCHEMY_DATABASE_URI")
+    SQLALCHEMY_DATABASE_URI = environ["SQLALCHEMY_DATABASE_URI"]
     SQLALCHEMY_ECHO = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
