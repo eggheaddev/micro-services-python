d1de1ba38bcd0d24f7e7fd3b37437aa0bd8bfbe9
slorard <slora@ipisdosaj.edu.do>
2020-09-24 14:42:36 -0400

    update & add (User model): change column name "name" to "username" & added a new column "storage_id"

--- src/models.py
+++ src/models.py
@@ -13,14 +13,15 @@ class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     public_id = db.Column(db.String(50), default=str(uuid.uuid4()), unique=True)
-    name = db.Column(db.String(70))
+    storage_id = password = db.Column(db.String(255), unique=True) 
+    username = db.Column(db.String(70), unique=True)
     email = db.Column(db.String(70),unique=True)
     password = db.Column(db.String(255))
     admin = db.Column(db.Boolean, default=False)
     created_at = db.Column(db.Date, default=_get_date)
 
-    def __init__(self, name, email, password):
-        self.name = name
+    def __init__(self, username, email, password):
+        self.username = username
         self.email = email
         self.password = password
 
@@ -28,7 +29,7 @@ class User(db.Model):
 
 class UserSchema(ma.Schema):
     class Meta:
-        fields = ('id', 'public_id', 'name', 'email', 'password')
+        fields = ('id', 'public_id', 'username', 'email', 'password', 'storage_id')
 
 
 user_schema = UserSchema()
