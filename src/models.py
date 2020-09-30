0d783b4ef6375fcb807b8ba9aa4b089b18b05c3f
RivierGrullon <rgrullon@ipisdosaj.edu.do>
2020-09-30 13:28:14 -0400

    feat: add Staff table and refact user table

--- src/models.py
+++ src/models.py
@@ -6,19 +6,21 @@ import uuid
 db = SQLAlchemy()
 ma = Marshmallow()
 
+
 def _get_date():
     return datetime.datetime.now()
 
-class User(db.Model):
-    __tablename__ = 'users'
+
+class abstractUser():
     id = db.Column(db.Integer, primary_key=True)
     public_id = db.Column(db.String(50), default=str(uuid.uuid4()), unique=True)
-    storage_id = password = db.Column(db.String(255), unique=True) 
     username = db.Column(db.String(70), unique=True)
-    email = db.Column(db.String(70),unique=True)
+    email = db.Column(db.String(70), unique=True)
     password = db.Column(db.String(255))
-    admin = db.Column(db.Boolean, default=False)
     created_at = db.Column(db.Date, default=_get_date)
+class User(db.Model, abstractUser):
+
+    __tablename__ = 'users'
 
     def __init__(self, username, email, password):
         self.username = username
@@ -26,11 +28,27 @@ class User(db.Model):
         self.password = password
 
 
-
 class UserSchema(ma.Schema):
     class Meta:
-        fields = ('id', 'public_id', 'username', 'email', 'password', 'storage_id')
+        fields = ('id', 'public_id', 'username', 'email', 'password')
 
 
 user_schema = UserSchema()
 users_schema = UserSchema(many=True)
+
+
+class Staff(db.Model, abstractUser):
+
+    __tablename__ = 'staff'
+    admin = db.Column(db.Boolean, default=True)
+    def __init__(self, username, email, password):
+
+        User.__init__(self, username, email, password)
+
+class StaffSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'public_id', 'username', 'email', 'password', 'admin')
+
+
+staff_schema = StaffSchema()
+staffs_schema = StaffSchema(many=True)
